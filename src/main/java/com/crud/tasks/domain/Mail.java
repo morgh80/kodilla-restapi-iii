92a2ac649f0d35dabeaa420784fd91188a39a270
Aeronaut <aeronaut@MacBook-Pro-Aeronaut-2.local>
2017-11-12 19:49:41 +0100

    Excersise 19.3 ready for review

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,21 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Mail {
+    @NonNull
+    private String mailTo;
+    private String toCc;
+    @NonNull
+    private String subject;
+    @NonNull
+    private String message;
+}
