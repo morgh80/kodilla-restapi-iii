dba9af5facdc4156dc7a0a12ca3cc2014e392aa7
aeronaut <aeronaut@MacBook-Pro-aeronaut.local>
2017-12-12 22:33:45 +0100

    Excersise 24.1 ready for review

--- src/main/java/com/crud/tasks/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -13,6 +13,10 @@ public class StaticWebPageController {
         model.put("variable", "My Thymeleaf variable");
         model.put("one", 1);
         model.put("two", 2);
+        model.put("equals", " = ");
+        model.put("add", " + ");
+        model.put("subtract", " + ");
+        model.put("multiply", " * ");
         return "index";
     }
 
